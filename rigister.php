b866c357aec9044b67da68f0892530033b3292f6
venkata dinesh kumar reddy <142785410+Venkatadinesh56@users.noreply.github.com>
2024-06-20 20:51:27 +0530

    Update rigister.php

--- rigister.php
+++ rigister.php
@@ -5,6 +5,7 @@ $db_hostname = "localhost";
 $db_username = "root";
 $db_password = "";
 $db_name = "proj";
+
 $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
 if (!$conn) {
     echo "connection failed:" . mysqli_connect_error();
